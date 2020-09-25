59bcc571ddbde0fdf6fc5d8b4e9b188890756859
Miki <merenyi.miklos5@gmail.com>
2020-09-25 14:01:44 +0200

    Ez lesz a második commit

--- app/src/main/java/com/example/a13t_git/MainActivity.java
+++ app/src/main/java/com/example/a13t_git/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //csak ez egy teszt
+    public void init()
+    {
+
+    }
 }
